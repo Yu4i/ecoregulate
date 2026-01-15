8bd0111de6abb450f5705350416721f7b305085d
Yuvraj <yuvmasoun@gmail.com>
2026-01-15 15:20:15 -0700

    Update Apollo Client to use production backend URL

---
+++ client/src/main.tsx
@@ -0,0 +1,34 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
+import { ApolloProvider } from '@apollo/client/react'
+import './index.css'
+import App from './App.tsx'
+
+const graphqlEndpoint = import.meta.env.PROD 
+  ? 'https://ecoregulate-backend-cvfscbhd2ga5c3.eastus-01.azurewebsites.net'
+  : 'http://localhost:4000'
+
+const client = new ApolloClient({
+  ssrMode: typeof window === 'undefined',
+  link: new HttpLink({
+    uri: graphqlEndpoint,
+  }),
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      errorPolicy: "all",
+    },
+    query: {
+      errorPolicy: "all",
+    },
+  },
+});
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </StrictMode>,
+)
